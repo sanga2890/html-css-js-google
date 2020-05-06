1487f01e7909874d225766ac5e3630397d22cdc2
Stefano <stefano2890@gmail.com>
2020-05-06 16:56:57 +0200

    style ook

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// cliccando sull'icona dell' app drawer dovrà apparire il dropdown menu;
+// ricliccando nuovamente sempre sulla stessa icona il dropdown menu dovra prendere il display none e tornare ad essere nascosto;
+// e così via;
     $('#header-right>ul>li>a>i').click(function(){
     $('.dropdown').toggleClass('visible');
 })
+
+// il dropdown menu dovrà sconparire anche quando clicco su un punto esterno e non necessariamente sull'icona dell'app drawer;
